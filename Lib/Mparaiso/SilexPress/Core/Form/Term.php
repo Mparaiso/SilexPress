64d07d5bf22a99ec92da9ca72c16887565689bed
mparaiso <aikah@free.fr>
2013-08-30 01:31:36 +0200

    update

--- Lib/Mparaiso/SilexPress/Core/Form/Term.php
+++ Lib/Mparaiso/SilexPress/Core/Form/Term.php
@@ -2,8 +2,11 @@
 
 namespace Mparaiso\SilexPress\Core\Form;
 
+use MongoId;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 
 class Term extends AbstractType
 {
@@ -20,10 +23,21 @@ class Term extends AbstractType
     }
 
     public function buildForm(FormBuilderInterface $builder, array $options)
-    {
+    { // add a post_parent field
         parent::buildForm($builder, $options);
+        $builder->addEventListener(FormEvents::POST_SET_DATA, function (FormEvent $event) {
+            $data = $event->getData();
+            $id = null;
+            if (isset($data["_id"])) {
+                $id = $data["_id"];
+            }
+            $event->getForm()->add("parent", "mongochoice", array("collection" => "terms",
+                "key" => "name",
+                "query" => array("taxonomy" => "category",
+                    "_id" => array('$ne' => new MongoId($id)))));
+        });
         $builder
             ->add("name")
-            ->add("description", "textarea",array("required"=>false));
+            ->add("description", "textarea", array("required" => false));
     }
 }
