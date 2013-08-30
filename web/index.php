e8bf84d16a43ec392082bf1663efbeef152b0eaf
mparaiso <aikah@free.fr>
2013-08-30 11:47:05 +0200

    update

--- web/index.php
+++ web/index.php
@@ -4,7 +4,6 @@ use App\Config;
 
 // DIRECTIVE FOR PHP built-in server
 
-
 $filename = __DIR__ . preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
 if (php_sapi_name() === 'cli-server' && is_file($filename)) {
     return false;
